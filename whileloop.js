74016b8108b7338324a9077a7cc547e0d71bf4c7
KhaingZinZinZaw2017 <khaingzinzinzaw2017@gmail.com>
2023-01-18 14:52:11 +0630

    while loop code

---
+++ whileloop.js
@@ -0,0 +1,11 @@
+// let dice = Math.trunc((Math.random() * 6))+ 1;
+// console.log(dice);
+// while(dice !== 6){
+//     console.log(`You rolled a ${dice}`);
+//     // try without below code
+//     dice = Math.trunc((Math.random() * 6))+ 1;
+//     if(dice === 6) console.log('Loop is stop');
+// }
+const birthYear = 2000;
+const age = birthYear => 2022 - birthYear
+console.log(age(birthYear));
